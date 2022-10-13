1d5ecfdabd2395386547864dbe2d53761c4f1b08
cvisbal0724 <cvisbal0724@gmail.com>
2022-10-12 19:01:55 -0500

    cambios badrequest

--- apps/api.lami/src/order/order.module.ts
+++ apps/api.lami/src/order/order.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { OrderService } from './order.service';
 import { OrderController } from './order.controller';
+import { CommonsModule } from '../commons/commons.module';
 
 @Module({
+  imports: [
+    CommonsModule
+  ],
   controllers: [OrderController],
-  providers: [OrderService]
+  providers: [OrderService],
+  exports: [OrderService]
 })
 export class OrderModule {}
