733e74e325209ada335693dc8578268b1df1014c
cvisbal0724 <cvisbal0724@gmail.com>
2022-10-13 10:48:18 -0500

    auxios timeout

--- apps/integration-sap/src/auth/auth.module.ts
+++ apps/integration-sap/src/auth/auth.module.ts
@@ -3,13 +3,17 @@ import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { HttpModule } from '@nestjs/axios';
 import { EasyconfigModule } from 'nestjs-easyconfig';
+import * as https from 'https';
 
 @Module({
   imports: [
     EasyconfigModule.register({ path: './.env' }),
     HttpModule.register({
       timeout: 50000,
-      maxRedirects: 5
+      maxRedirects: 5,
+      httpsAgent: new https.Agent({
+        rejectUnauthorized: false
+      })
     }),
   ],
   controllers: [AuthController],
