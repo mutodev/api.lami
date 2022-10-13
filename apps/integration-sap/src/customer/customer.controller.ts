6b01add14bfe0d691f04adfa8035b67ed6689292
cvisbal0724 <cvisbal0724@gmail.com>
2022-10-13 14:54:11 -0500

    cambios sap clientes

--- apps/integration-sap/src/customer/customer.controller.ts
+++ apps/integration-sap/src/customer/customer.controller.ts
@@ -1,15 +1,18 @@
 import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
 import { Ctx, MessagePattern, Payload, RedisContext } from '@nestjs/microservices';
+import { AuthService } from '../auth/auth.service';
 import { CustomerService } from './customer.service';
 import { CreateCustomerDto } from './dto/create-customer.dto';
 import { UpdateCustomerDto } from './dto/update-customer.dto';
 
 @Controller('customer')
 export class CustomerController {
-  constructor(private readonly customerService: CustomerService) {}
+  constructor(private readonly customerService: CustomerService,
+              private authService: AuthService) {}
 
   @Get(':id')
   async test(@Param() id: string) {
+    await this.authService.login();
     return await this.customerService.findOne(id);
   }
 
