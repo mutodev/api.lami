c85598f7aa4e75970ea09c30b66b3a906559eaa5
cvisbal0724 <cvisbal0724@gmail.com>
2022-10-14 08:40:05 -0500

    cambios sap clientes

--- apps/integration-sap/src/customer/customer.controller.ts
+++ apps/integration-sap/src/customer/customer.controller.ts
@@ -1,6 +1,7 @@
 import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
 import { Ctx, MessagePattern, Payload, RedisContext } from '@nestjs/microservices';
 import { AuthService } from '../auth/auth.service';
+import { PrismaService } from '../commons/prisma.service';
 import { CustomerService } from './customer.service';
 import { CreateCustomerDto } from './dto/create-customer.dto';
 import { UpdateCustomerDto } from './dto/update-customer.dto';
@@ -8,12 +9,22 @@ import { UpdateCustomerDto } from './dto/update-customer.dto';
 @Controller('customer')
 export class CustomerController {
   constructor(private readonly customerService: CustomerService,
+              private prismaService: PrismaService,
               private authService: AuthService) {}
 
   @Get(':id')
-  async test(@Param('id') id: string) {
+  async byId(@Param('id') id: string) {
     await this.authService.login();
-    return await this.customerService.findAll();
+    return await this.customerService.findOne(id);
+  }
+
+  @Get('migrate/:id')
+  async migrate(@Param('id') id: string) {
+    await this.authService.login();
+    const result = await this.customerService.findAll();
+    result.data.value.map(async (c) => {
+      this.prismaService.customer.findFirst({where: {identification: c.CardCode}});
+    })
   }
 
   @MessagePattern('customer/create')
