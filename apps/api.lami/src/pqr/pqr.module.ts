73427bd3928af8c2a3bea72556b553375ba368a0
cvisbal0724 <cvisbal0724@gmail.com>
2023-03-13 21:52:44 -0500

    cambios pqr

--- apps/api.lami/src/pqr/pqr.module.ts
+++ apps/api.lami/src/pqr/pqr.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { PqrService } from './pqr.service';
 import { PqrController } from './pqr.controller';
+import { CommonsModule } from '../commons/commons.module';
 
 @Module({
+  imports: [
+    CommonsModule
+  ],
   controllers: [PqrController],
-  providers: [PqrService]
+  providers: [PqrService],
+  exports: [PqrService]
 })
 export class PqrModule {}
