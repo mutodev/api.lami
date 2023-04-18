d2c64e2cf8667be65a7209cf5ffc91b06f690abe
cvisbal0724 <cvisbal0724@gmail.com>
2023-04-18 14:21:09 -0500

    se creo el socket para ver los pedidos creados

--- apps/api.lami/src/commons/streams/actions-order.ts
+++ apps/api.lami/src/commons/streams/actions-order.ts
@@ -2,3 +2,4 @@ import { Subject } from "rxjs";
 
 export const seeEventOrderStream = new Subject<any>();
 export const seeEventCustomerStream = new Subject<any>();
+export const seeEventOrderCreatedStream = new Subject<any>();
