8958e7e4388c4b9a75b5a1f7e0329de82591dae6
cvisbal0724 <cvisbal0724@gmail.com>
2023-04-18 13:56:12 -0500

    se agregaron los campos de clientes

--- apps/api.lami/src/customer/dto/create-customer.dto.ts
+++ apps/api.lami/src/customer/dto/create-customer.dto.ts
@@ -76,9 +76,20 @@ export class CreateCustomerDto {
 
     @ApiProperty()
     @IsOptional()
+    @IsString()
+    address2?: string;
+
+    @ApiProperty()
+    @IsOptional()
     @MaxLength(30, {message: 'La cantidad de caracteres maximos permitodos en el telefón son 30.'})
     @IsString()
     phone?: string;
+
+    @ApiProperty()
+    @IsOptional()
+    @MaxLength(30, {message: 'La cantidad de caracteres maximos permitodos en el telefón son 30.'})
+    @IsString()
+    phone2?: string;
 
     @ApiProperty()
     @IsOptional()
